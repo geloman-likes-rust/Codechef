ab631e82b91da3aefd34c43380117e3ecb7702cb
geloman <geloman-likes-rust@users.noreply.github.com>
2023-05-03 01:02:44 +0800

    Counting Words

---
+++ CNTWRD.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int T;
+  cin >> T;
+  while (T--) {
+    int N, M;
+    cin >> N >> M;
+    cout << N * M << "\n";
+  }
+
+  return 0;
+}
