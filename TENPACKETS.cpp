f7c2e46d7cb4274d26fa4a947e023f60ab71c50f
geloman <geloman-likes-rust@users.noreply.github.com>
2023-05-06 21:47:22 +0800

    Buy Ten Packets

---
+++ TENPACKETS.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int T;
+  cin >> T;
+  while (T--) {
+    int X, Y;
+    cin >> X >> Y;
+    cout << X + Y * 2 << "\n";
+  }
+  return 0;
+}
